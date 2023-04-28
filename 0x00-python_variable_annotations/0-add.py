682da365610cf126f7479c1978bc6313893aff95
patoctd4real <patoctd4real@yahoo.com>
2023-04-28 22:40:15 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+add.py
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    float a and a float b as arguments and returns their sum as a float.
+    """
+    return a + b
+
+outPut = add(1.2,1.3)
+print(outPut)
