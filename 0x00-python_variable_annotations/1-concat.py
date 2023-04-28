68a13e3ed0df6a53d4a47a065e345b4744362dde
patoctd4real <patoctd4real@yahoo.com>
2023-04-28 23:07:42 +0000

    python tasks

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''1-concat.py
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''Concatenating 2(two)strings.
+    '''
+    return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 2's module.
+'''
+
+
+def floor(p: float) -> int:
+    '''take floor and return floating number.
+    '''
+    return int(p)
