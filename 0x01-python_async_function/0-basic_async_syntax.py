273d40660108a709c62cf3de6a9e65a9a516cb3f
patoctd4real <patoctd4real@yahoo.com>
2023-05-03 03:26:25 +0000

    correction

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-'''Task 0' my module tasks.
+'''Task 0' asynchronous coroutine.
 '''
 import asyncio
 import random
-
-
 async def wait_random(max_delay: int = 10) -> float:
     '''Waits for some second before random number.
     '''
-    show_time = random.random() * max_delay
-    await asyncio.sleep(show_time)
-    return show_time
+    rand = random.random() * max_delay
+    await asyncio.sleep(rand)
+    return rand
